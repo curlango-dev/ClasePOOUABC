4cde09888bce3098c06a1d3d1852d46273a3fe43
curlango-dev <curlango@uabc.edu.mx>
2025-09-10 15:15:00 -0700

    Add files via upload
    Se corrigió la interfaz de usuario. 
    Muestra la cantidad de dados según la cantidad de fichas que tiene el jugador actual.
    Cuando 2 jugadores se quedan si fichas, se deshabilita el botón para que ya no se pueda lanzar.
    Los jugadores empiezan con 3 fichas cada uno.

--- Left-Center-Right/LCRGameGUI.java
+++ Left-Center-Right/LCRGameGUI.java
@@ -14,12 +14,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LCRGameGUI extends Application {
-    LCRGame lcrGame;
-    Label fichasCenter;
-    Label jugador1;
-    Label jugador2;
-    Label jugador3;
-    Label jugadorActual;
+    private LCRGame lcrGame;
+    private Label fichasCenter;
+    private Label fichasJugador1;
+    private Label fichasJugador2;
+    private Label fichasJugador3;
+    private Label jugadorActual;
+    private Label dado1;
+    private Label dado2;
+    private Label dado3;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -56,16 +59,16 @@ public class LCRGameGUI extends Application {
         panel.getChildren().add(titulo);
         switch (numeroDeJugador) {
             case 1:
-                jugador1 = new Label("0");
-                panel.getChildren().add(jugador1);
+                fichasJugador1 = new Label("3");
+                panel.getChildren().add(fichasJugador1);
                 break;
             case 2:
-                jugador2 = new Label("0");
-                panel.getChildren().add(jugador2);
+                fichasJugador2 = new Label("3");
+                panel.getChildren().add(fichasJugador2);
                 break;
             case 3:
-                jugador3 = new Label("0");
-                panel.getChildren().add(jugador3);
+                fichasJugador3 = new Label("3");
+                panel.getChildren().add(fichasJugador3);
                 break;
         }
 
@@ -77,19 +80,19 @@ public class LCRGameGUI extends Application {
         HBox centro = new HBox();
 
         Font font = new Font("Arial", 20);
-        Label dado1 = new Label("L");
+        dado1 = new Label("L");
         dado1.setFont(font);
         dado1.setStyle("-fx-border-color: blue; " +
                 "-fx-border-width: 2; " +
                 "-fx-border-style: solid; " +
                 "-fx-padding: 5;");
-        Label dado2 = new Label("C");
+        dado2 = new Label("C");
         dado2.setFont(font);
         dado2.setStyle("-fx-border-color: blue; " +
                 "-fx-border-width: 2; " +
                 "-fx-border-style: solid; " +
                 "-fx-padding: 5;");
-        Label dado3 = new Label("R");
+        dado3 = new Label("R");
         dado3.setFont(font);
         dado3.setStyle("-fx-border-color: blue; " +
                 "-fx-border-width: 2; " +
@@ -98,32 +101,74 @@ public class LCRGameGUI extends Application {
 
         jugadorActual = new Label("0");
         jugadorActual.setFont(font);
-        jugadorActual.setStyle("-fx-border-color: blue; " +
-                "-fx-border-width: 2; " +
-                "-fx-border-style: solid; " +
-                "-fx-padding: 5;");
+//        jugadorActual.setStyle("-fx-border-color: blue; " +
+//                "-fx-border-width: 2; " +
+//                "-fx-border-style: solid; " +
+//                "-fx-padding: 5;");
+        jugadorActual.setVisible(false);
         Button lanzar = new Button("Lanzar");
         lanzar.setOnAction(e -> {
-            lcrGame.lanzarDados();
-            lcrGame.procesarResultados();
-            lcrGame.cambiarTurno();
-            lcrGame.establecerJugadores();
-
-            dado1.setText("" + lcrGame.getDado1().getValor());
-            dado2.setText("" + lcrGame.getDado2().getValor());
-            dado3.setText("" + lcrGame.getDado3().getValor());
-
-            jugador1.setText("" + lcrGame.getJugador1().getFichas());
-            jugador2.setText("" + lcrGame.getJugador2().getFichas());
-            jugador3.setText("" + lcrGame.getJugador3().getFichas());
-
-            jugadorActual.setText("" + lcrGame.getJugadorActual().getNombre());
-
-            fichasCenter.setText("" + lcrGame.getCentro().getFichas());
+            atenderLanzamiento();
+            if (lcrGame.esFinDeJuego()) {
+                // cuando se termina el juego
+                lanzar.setDisable(true); // deshabilita el botón
+            }
         });
         centro.setAlignment(Pos.CENTER);
         centro.getChildren().addAll(lanzar, dado1, dado2, dado3, jugadorActual);
         return centro;
+    }
+
+    private void atenderLanzamiento() {
+        lcrGame.lanzarDados();
+        lcrGame.procesarResultados();
+        lcrGame.cambiarTurno();
+        lcrGame.establecerJugadores();
+
+        actualizarDados();
+        actualizarFichasDeJugadores();
+
+        jugadorActual.setVisible(true);
+        jugadorActual.setText("" + lcrGame.getJugadorActual().getNombre());
+
+        fichasCenter.setText("" + lcrGame.getCentro().getFichas());
+    }
+
+    private void actualizarFichasDeJugadores() {
+        fichasJugador1.setText("" + lcrGame.getJugador1().getFichas());
+        fichasJugador2.setText("" + lcrGame.getJugador2().getFichas());
+        fichasJugador3.setText("" + lcrGame.getJugador3().getFichas());
+    }
+
+    private void actualizarDados() {
+        dado1.setText("" + lcrGame.getDado1().getValor());
+        dado2.setText("" + lcrGame.getDado2().getValor());
+        dado3.setText("" + lcrGame.getDado3().getValor());
+
+        // mostrar solo la cantidad de dados que va a lanzar
+        //  el jugador actual
+        switch (lcrGame.getJugadorActual().getFichas()) {
+            case 0: // si no tiene fichas, muestra 0 dados
+                dado1.setVisible(false);
+                dado2.setVisible(false);
+                dado3.setVisible(false);
+                break;
+            case 1: // si tiene una ficha, muestra 1 dado
+                dado1.setVisible(true);
+                dado2.setVisible(false);
+                dado3.setVisible(false);
+                break;
+            case 2: // si tiene 2 fichas, muestra 2 dados
+                dado1.setVisible(true);
+                dado2.setVisible(true);
+                dado3.setVisible(false);
+                break;
+            default: // si tiene 3 o mas fichas, muestra 3 dados
+                dado1.setVisible(true);
+                dado2.setVisible(true);
+                dado3.setVisible(true);
+                break;
+        }
     }
 
     public static void main(String[] args) {
